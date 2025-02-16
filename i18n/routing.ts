30158b053b00ed39a36e8a82b7d9acb304834dde
AdanSerrano <adan.serrano@utp.ac.pa>
2025-02-16 16:27:17 +0100

    fix(RESOLVE FIX): :bug: RESOLVE FIX IN THE SELECT BY CHANGE LANGUAGE

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -4,7 +4,7 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
     locales: ['es', 'en', 'de', 'ru'],
     defaultLocale: 'es',
-    localePrefix: 'never' // but you can use 'never' and 'always' for show or not locale prefix
+    localePrefix: 'always'
 });
 
 export const { Link, redirect, usePathname, useRouter, getPathname } =
